fa086fdb7ad118b6eb93c15f8e8388c6cae83603
Keesun Baik <keesun.baik@navercorp.com>
2015-05-12 19:10:25 +0900

    using custom resource handler

---
+++ sbt-webmvc/src/main/java/me/whiteship/configs/WebMvcConfiguration.java
@@ -0,0 +1,19 @@
+package me.whiteship.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author Keeun Baik
+ */
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(0);
+    }
+}
