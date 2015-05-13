2ee187b4eb35a535ddfe84d6e9dba8b7e116e136
Keesun Baik <keesun.baik@navercorp.com>
2015-05-13 19:00:01 +0900

    using jdbc

---
+++ sbt-jdbc/src/main/java/me/whiteship/daos/AccountRepository.java
@@ -0,0 +1,32 @@
+package me.whiteship.daos;
+
+import me.whiteship.domains.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Keeun Baik
+ */
+@Repository
+public class AccountRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public void add(Account account) {
+        jdbcTemplate.update("insert into account (id, username, password) values (?, ?, ?)",
+                account.getId(), account.getUsername(), account.getPassword());
+    }
+
+    public Account get(long id) {
+        return jdbcTemplate.queryForObject("select * from account where id = ?", new Object[]{id}, (rs, rowNum) -> {
+            Account account = new Account();
+            account.setId(rs.getLong("id"));
+            account.setUsername(rs.getString("username"));
+            account.setUsername(rs.getString("password"));
+            return account;
+        });
+    }
+
+}
